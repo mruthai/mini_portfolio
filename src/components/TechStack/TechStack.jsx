9bd1f264383b8c0872feb343cbda06b8bc3d904e
Martin Ruthaivilavan <mmvilavan@gmail.com>
2023-09-10 09:03:10 -0500

    icon color update

--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 import { DiJsBadge, DiReact, DiVisualstudio, DiHtml5, DiCss3, DiDjango, DiFirebase, DiGit, DiMongodb } from 'react-icons/di';
-import { SiFlask, SiJson, SiTailwindcss } from 'react-icons/si';
+import { SiFlask, SiJson, SiTailwindcss, SiNetlify } from 'react-icons/si';
 import { FaPython, FaNode, FaBootstrap } from 'react-icons/fa';
 import { BiLogoPostgresql, BiLogoGithub } from 'react-icons/bi'
 
 const TechStack = () => {
     return (
-        <div className="container">
+        <div className="container ">
             <h1 className="text-center text-5xl my-5">Tech Stack</h1>
-            <div className=" flex flex-row text-5xl justify-center gap-5 my-5" >
-                <DiJsBadge />
-                <DiReact />
-                <DiVisualstudio />
+            <div className="flex flex-rol justify-center gap-5 my-10 ">
+                <div className=" text-4xl p-2" >
+                    <DiJsBadge style={{color: 'F0DB4F', backgroundColor:'323330'}} />
+                    <DiReact style={{color: '149eca'}}/>
+                    <DiVisualstudio />
+                    <DiGit style={{color:'red'}}/>
+                    <BiLogoGithub />
+                    <DiHtml5 />
 
-            </div>
-            <div className=" flex flex-row text-5xl justify-center gap-5 my-5">
-                <DiGit />
-                <BiLogoGithub />
-                <DiHtml5 />
-                <DiCss3 />
-                
-            </div>
-            <div className=" flex flex-row text-5xl justify-center gap-5 my-5">
-                <FaNode />
-                <SiTailwindcss />
-                < FaBootstrap />
-                <DiDjango />
-               
-            </div>
-            <div className=" flex flex-row text-5xl justify-center gap-5 my-5">
-            <DiFirebase />
-                <SiFlask />
-                <FaPython />
-                <DiMongodb />
-                < SiJson />
-                < BiLogoPostgresql />
+                </div>
+                <div className="text-4xl p-2">
+                    <SiTailwindcss />
+                    <DiCss3 />
+                    <FaNode />
+                    < FaBootstrap />
+                    <DiDjango />
+                    <DiFirebase />
+                </div>
+                <div className="text-4xl p-2">
+                    <SiFlask />
+                    <FaPython style={{color: 'greenblue'}}/>
+                    <DiMongodb />
+                    <SiJson />
+                    <BiLogoPostgresql />
+                    <SiNetlify />
+
+                </div>
             </div>
         </div>
     )
